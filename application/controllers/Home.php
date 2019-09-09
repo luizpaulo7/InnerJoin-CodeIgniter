0eac84c748361d27aa5e0a95ae8e17c31cfc2b68
Luiz Paulo <37484860+luizpaulo7@users.noreply.github.com>
2019-09-09 20:18:45 -0300

    Update Home.php

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1,33 +1,25 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Home extends CI_Controller {
+class Home extends CI_Controller
+{
 
 	public function index()
 	{
-		$cursos = $this->db
-					->select('*')
-					->from('Curso')
-					->get()
-					->result();
+		$this->load->model('aluno');
+		$this->load->model('curso');
 
-		$alunos = $this->db
-					->select('a.*, c.nome AS nomeCurso')
-					->from('Aluno AS a')
-					->join('AlunoCurso AS ac', 'ac.idAluno = a.idAluno')
-					->join('Curso AS c', 'c.idCurso = ac.idCurso')
-					->get()
-					->result();
-		
 		$data['title'] = 'LISTA DE CLIENTES';
 
-		$data['alunos'] = $alunos;
-		$data['cursos'] = $cursos;
+		$data['alunos'] = $this->aluno->getAll();
+		$data['cursos'] = $this->curso->getAll();
+
+		$data['aluno'] = $this->aluno->getById(2);
 
 		$this->load->view('includes/header', $data);
 
 		$this->load->view('cliente/lista', $data);
-		
+
 		$this->load->view('includes/footer');
 	}
 }
